377e3a6c446a88db994e8e160e5aac2a1d663080
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-17 15:37:33 -0400

    Program now compares the city entered and its associated temperature.

--- weather-app.py
+++ weather-app.py
@@ -1,10 +1,11 @@
 import requests
 from pprint import *
-import tkinter as tk
 import matplotlib.pyplot as plt
 import numpy as np
 
 places = []
+templist = []
+lol = []
 API_Key = 'f276904f74631c4b0c2a0ae932d44124'
 cit = input("How Many cities do you want to compare? ")
 for i in range(int(cit)):
@@ -12,29 +13,18 @@ for i in range(int(cit)):
     base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
     weather_data = requests.get(base_url).json()
     weather_data.items()
+    places.append(city.title())
+    lol.append(0.5)
     for key, value in weather_data.items():
         if key == "main":
-            #print(value.split())
             temp_pair = value.items()
             for j,k in temp_pair:
                 if j == "temp":
+                    ctemp = k-273.15
                     print(k-273.15)
-            #print(value)
-    #places.append(city)
+                    templist.append(ctemp)
 
-# city = input("Enter a city: ")
+fig, ax = plt.subplots()
 
-# base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
-
-# weather_data = requests.get(base_url).json()
-
-#pprint(weather_data)
-
-#print(weather_data.items())
-
-
-
-# window = tk.Tk()
-# window.mainloop()
-# greeting = tk.Label(text="Hello, Tkinter")
-# greeting.pack()
+ax.scatter(x=places, y=templist, c=lol, s=np.abs(lol)*500)
+plt.show()
